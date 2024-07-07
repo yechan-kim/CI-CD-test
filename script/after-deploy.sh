c30ab78c22e96cab5131f47eafb3702a9826ef61
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-07-08 02:44:39 +0900

    실행 로직 변경

--- script/after-deploy.sh
+++ script/after-deploy.sh
@@ -1,11 +1,22 @@
-#!/bin/bash
-REPOSITORY=/home/api/
+#!/usr/bin/env bash
 
-cd $REPOSITORY/api_back
+REPOSITORY=/home/ubuntu/moduform
+cd $REPOSITORY
 
-echo "> Stop & Remove docker services."
-cd ..
-docker compose down
+APP_NAME=moduform
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-echo "> Run new docker services."
-docker compose up --build -d
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할 애플리케이션이 없습니다."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> Deploy - $JAR_PATH "
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
